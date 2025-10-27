f89620eafc2445c32db0bda1e58829498f154871
Sayan014 <90103006+Sayan014@users.noreply.github.com>
2025-10-27 19:38:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
-    //Write your code here
+const fname = document.querySelector('input[name="fname"]').value.trim();
+    const lname = document.querySelector('input[name="lname"]').value.trim();
+
+    alert(`${fname} ${lname}`);
 
 }
